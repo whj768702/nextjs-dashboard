54a87ebd2f31487ba8ac40e3fae5d2382aff351c
whj768702 <whj768702@gmail.com>
2024-06-25 17:41:13 +0800

    feat(dashboard): add loading skeletons and suspend fetching
    - Implement DashboardSkeleton component for loading states.
    - Refactor page.tsx to use Suspense for lazy loading.
    - Move fetching logic to data.ts and remove console logs.
    - Simplify LatestInvoices and RevenueChart components.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,11 +2,11 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { fetchCardData } from '@/app/lib/data';
+import { Suspense } from 'react';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
   const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
   return (
     <main>
@@ -20,8 +20,12 @@ export default async function Page() {
         <Card title='Total Customers' value={numberOfCustomers} type='customers'></Card>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}></RevenueChart>
-        {<LatestInvoices latestInvoices={latestInvoices}></LatestInvoices>}
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart></RevenueChart>
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices></LatestInvoices>
+        </Suspense>
       </div>
     </main>
   );
