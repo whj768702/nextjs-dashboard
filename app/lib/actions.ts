4f1fa89e0788e6a5a1ed398902229b59329e48c4
whj768702 <whj768702@gmail.com>
2024-06-26 16:15:10 +0800

    feat(auth): implement sign in and sign out functionality with NextAuth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,8 @@ import { sql } from '@vercel/postgres';
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -106,4 +108,20 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+export async function authenticate(prevState: string | undefined, formData: FormData) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials';
+        default:
+          return 'Something went wrong';
+      }
+    }
+    throw error;
+  }
 }
